350f3ff6f0801582d1364acd848e31be61f135f3
Drew <dmf2990@gmail.com>
2023-12-12 14:37:04 -0500

    simple syntax

--- hello.py
+++ hello.py
@@ -1,11 +1,54 @@
 colors = "red,blue,green,orange"
+thislist = ["apple", "banana", "cherry"]
 
 transform = colors.split(",", 2)
 
 print(transform)
+print(thislist)
+print("Hello Drew")
+print("More \ntext")
 
 
+# simply iterate through the list to see what is in it
+thislist = ["apple", "banana", "cherry"]
+for x in thislist:
+    print(x)
+
+#iterate through a string
+word = "Testytesterson"
+for x in word:
+   print(x)
+
+#add a simple condition
+fruits = "apple", "banana", "cherry"
+for x in fruits:
+  if x == "banana":
+    break  
+  print(x)
 
 # PS C:\Users\Drew\Desktop\dev\python\flask_postgres> py hello.py
 
 # https://www.python-engineer.com/posts/string-methods-python/
+
+# Create new github repo > get url for repo > git init > git add > git commit -m "" > 
+# git remote add origin <url> > set upstream > git push
+
+#if statement
+if a > b: print("a is greater than b")
+
+#if elseif else long version
+a = 200
+b = 33
+if b > a:
+  print("b is greater than a")
+elif a == b:
+  print("a and b are equal")
+else:
+  print("a is greater than b")
+
+
+#while loop
+i = 1
+while i < 6:
+  print(i)
+  i += 1
