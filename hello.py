e3cc0d173555d99d94515b4170eed26c1bc5a076
Drew <dmf2990@gmail.com>
2023-12-12 13:48:54 -0500

    first commit hello world

---
+++ hello.py
@@ -0,0 +1,11 @@
+colors = "red,blue,green,orange"
+
+transform = colors.split(",", 2)
+
+print(transform)
+
+
+
+# PS C:\Users\Drew\Desktop\dev\python\flask_postgres> py hello.py
+
+# https://www.python-engineer.com/posts/string-methods-python/
